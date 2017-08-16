03b261ecb2e95f9e3268a9a88b26439ad94ed638
Vishnu Gopalan <vishnu.gopalan@emids.com>
2017-08-16 18:56:06 +0530

    Hra - Module

---
+++ src/pages/hra/hra.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'hra.html'
+})
+export class HraPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
