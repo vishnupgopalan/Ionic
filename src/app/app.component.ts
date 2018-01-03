3f0117532a47bda80841684122e52e53a4dae4a9
vishnupgopalan <vishnupg_dev@hotmail.com>
2018-01-04 00:29:56 +0530

    Added Cordova plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ import { LogoutPage } from '../pages/Logout/logout';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = Login;
 
   pages: Array<{title: string, component: any}>;
 
